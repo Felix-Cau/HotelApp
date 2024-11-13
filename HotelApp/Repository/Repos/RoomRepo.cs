a9359252d3d0028c848ab7a013669884c731be07
Felix Cau <130475694+Felix-Cau@users.noreply.github.com>
2024-11-13 09:16:47 +0100

    Fixed faulty logical condition.

--- HotelApp/Repository/Repos/RoomRepo.cs
+++ HotelApp/Repository/Repos/RoomRepo.cs
@@ -15,7 +15,7 @@ namespace HotelApp.Repository.Repos
         public List<Room> SearchAvailableRooms(DateTime startDate, DateTime endDate, int guests)
         {
             var unavailableRooms = _context.Bookings.Include(b => b.Room).ThenInclude(r => r.RoomType)
-                                                    .Where(b => guests > (b.Room.RoomType.RoomSize + b.Room.RoomType.ExtraBeds) && (b.CheckInDate <= endDate && b.CheckOutDate >= startDate))
+                                                    .Where(b => guests > (b.Room.RoomType.RoomSize + b.Room.RoomType.ExtraBeds) || (b.CheckInDate <= endDate && b.CheckOutDate >= startDate))
                                                     .Select(b => b.Room).AsNoTracking().ToList();
 
             var availableRooms = _context.Rooms.Include(r => r.RoomType).Where(r => !unavailableRooms.Contains(r) && (r.RoomType.RoomSize + r.RoomType.ExtraBeds) >= guests).AsNoTracking().ToList();
